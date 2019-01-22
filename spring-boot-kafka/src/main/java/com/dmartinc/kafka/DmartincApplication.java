a404f3e57b62c9da09ce44b5b5e596552871cecd
Daniel Martin <daniel.martin@travel-ims.com>
2019-01-22 19:49:05 +0100

    Upgrade gradlew 5.1.1 & add spring-boot-kafka module

---
+++ spring-boot-kafka/src/main/java/com/dmartinc/kafka/DmartincApplication.java
@@ -0,0 +1,14 @@
+package com.dmartinc.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmartincApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmartincApplication.class, args);
+	}
+
+}
+
