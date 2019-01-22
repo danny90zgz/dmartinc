e6adb7e3590f4c7733deb5e316fd954aac88a764
Daniel Martin <daniel.martin@travel-ims.com>
2019-01-22 20:24:44 +0100

    Add configurations

--- spring-boot-kafka/src/test/java/com/dmartinc/kafka/KafkaApplicationTests.java
+++ spring-boot-kafka/src/test/java/com/dmartinc/kafka/KafkaApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DmartincApplicationTests {
+public class KafkaApplicationTests {
 
 	@Test
 	public void contextLoads() {
